ebc4abc1e101eb93a2369da91cf17198a0a84703
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-12 12:01:45 +0300

    added to app.py

--- server/app.py
+++ server/app.py
@@ -1,5 +1,16 @@
-#!/usr/bin/env python3
 
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
